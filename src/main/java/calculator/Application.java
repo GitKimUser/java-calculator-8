c566238f45babf9dfbc7545741517413507a9876
GitKimUser <kmg6488@gmail.com>
2025-10-17 22:44:50 +0900

    feat(calculator): 쉼표와 콜론을 구분자로 사용해 숫자 합산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = 0;
+        StringCalculator calculator = new StringCalculator();
+
+        result = calculator.add(input);
+
+        System.out.printf("결과 : %d\n", result);
+    }
+}
+
+class StringCalculator {
+    public int add(String text){
+        if(text.isEmpty()){
+            return 0;
+        }
+        String[] tokens = text.split("[,:]");
+        int sum = 0;
+        for(int i = 0 ; i < tokens.length; i++){
+            sum += Integer.parseInt(tokens[i]);
+        }
+
+        return sum;
     }
 }
